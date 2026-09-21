3a5784e66d0d3e7b2742c45bf1bfb201f23a65ee
Marok07 <karsai.marko.2022d@telekimezotur.hu>
2026-09-21 11:28:23 +0200

    02_RaktarApp

--- 02_RaktarApp/Program.cs
+++ 02_RaktarApp/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using _02_RaktarApp;
+
+Termek LGK = new Termek("Logitech G19 Gaming Keyboard", 67900);
+Termek LGM = new Termek("Logitech G9 Gaming Laser Mouse", 25000);
+Termek LGH = new Termek("Logitech G35 Gaming Headset", 20000);
+
+Console.WriteLine($"Összesen létrehozott termék: {Termek.TermekSzamlalo} db");
---
+++ 02_RaktarApp/Termek.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02_RaktarApp
+{
+    internal class Termek
+    {
+        public string Nev { get; set; }
+        public int EgysegAr { get; private set; }
+
+        public static int TermekSzamlalo = 0;
+
+        public Termek(string n, int ea)
+        {
+            this.Nev = n;
+            this.EgysegAr = ea;
+
+            TermekSzamlalo = TermekSzamlalo + 1;
+        }
+
+    }
+}
