aa75deb4df018dd47cf4e2023a406d2968c71811
Marok07 <karsai.marko.2022d@telekimezotur.hu>
2026-09-21 11:18:35 +0200

    01_KonyvtarApp

---
+++ 01_KonyvtarApp/Konyv.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_KonyvtarApp
+{
+    internal class Konyv
+    {
+        public readonly string Cim;
+        private int oldalszam;
+        public int Oldalszam {
+            get { return oldalszam; }
+            set
+            {
+                if ( value>0 )
+                {
+                    oldalszam = value;
+                }
+            } 
+        }
+        public Konyv(string c, int o)
+        {
+            this.Cim = c;
+            this.oldalszam = o;
+        }
+
+        public override string ToString()
+        {
+            return $"Könyv: {Cim}, {Oldalszam} oldal";
+        }
+
+    }
+}
---
+++ 01_KonyvtarApp/Program.cs
@@ -0,0 +1,6 @@
+﻿using _01_KonyvtarApp;
+
+Konyv ECs = new Konyv("Egyri Csillagok",500);
+ECs.Oldalszam = -50;
+
+Console.WriteLine(ECs.ToString());
