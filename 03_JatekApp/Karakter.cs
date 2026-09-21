8e4ca7b34440c99a925be70a06162ec5bfe51535
Marok07 <karsai.marko.2022d@telekimezotur.hu>
2026-09-21 11:37:58 +0200

    03_JatekApp

---
+++ 03_JatekApp/Karakter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03_JatekApp
+{
+    internal class Karakter
+    {
+        public string Nev { get; private set; }
+        //private int eletero;
+        public int Eletero { get; private set; }
+        
+
+        public Karakter(string n)
+        {
+            this.Nev = n;
+            this.Eletero = 100;
+        }
+
+        public void Sebzodik(int pont)
+        {
+            Eletero -= pont;
+            if (Eletero < 0) Eletero = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nev} - Életerő: {Eletero} HP";
+        }
+
+    }
+}
---
+++ 03_JatekApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using _03_JatekApp;
+
+Karakter Jani = new Karakter("Jani a Károly");
+Jani.Sebzodik(30);
+Console.WriteLine(Jani.ToString());
+Jani.Sebzodik(90);
+Console.WriteLine(Jani.ToString());
